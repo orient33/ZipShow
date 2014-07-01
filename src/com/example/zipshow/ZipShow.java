3f8d610f8dfd7f5ff0d914fde1c509fbbd280cdf
temp <dongfang.dun@singuloid.com>
2014-07-01 11:53:04 +0800

    v1.1 fix bugs: caches, and sort string/dirs

--- src/com/example/zipshow/ZipShow.java
+++ src/com/example/zipshow/ZipShow.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,39 +26,65 @@ import android.widget.TextView;
 
 public class ZipShow extends Activity {
 
+	final String sep = File.separator;
 	String mZipPath, mParent;
 	ListView mListView;
 	ArrayList<String> mAllDirs = new ArrayList<String>();
 	ArrayList<String> mDirs = new ArrayList<String>();
 	HashMap<String, ArrayList<String>> mCaches = new HashMap<String, ArrayList<String>>();
+	ProgressDialog mDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.zip_show);
 		mListView = (ListView) findViewById(R.id.zip_shou);
+		mDialog = new ProgressDialog(this);
+		mDialog.setCanceledOnTouchOutside(false);
+		mDialog.setCancelable(true);
 		mZipPath = "/sdcard/wfe.zip";
 		mListView.setAdapter(adapter);
 		mListView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> av, View v, int p, long id) {
-				Log.d("dfdun", "click " + v);
+				logd("click " + v);
 				TextView tv = (TextView) ((LinearLayout) v)
 						.findViewById(R.id.item);
 				String text = tv.getText().toString();
-				if (isDirectory(text))
-					new MyTask().execute(text);
+				if (isDirectory(text)) {
+					if (TextUtils.isEmpty(mParent) || sep.equals(mParent))
+						new MyTask().execute(text);
+					else
+						new MyTask().execute(mParent + text);
+				}
 			}
 		});
 		new MyTask().execute("");
 	}
 
 	@Override
+	protected void onStop() {
+		super.onStop();
+		if (mDialog != null && mDialog.isShowing())
+			mDialog.dismiss();
+	}
+
+	@Override
 	public void onBackPressed() {
-		if (mParent != null && mParent.lastIndexOf(File.separator) != -1) {
-			String sub = mParent.substring(0,
-					mParent.lastIndexOf(File.separator));
-			new MyTask().execute(sub);
+		if (sep.equals(mParent)) {
+			super.onBackPressed();
+		} else if (mParent != null && mParent.lastIndexOf(File.separator) != -1) {
+			int index = mParent.indexOf(File.separatorChar), lastindex = mParent
+					.lastIndexOf(File.separatorChar);
+			if (index == lastindex) // only 1 separator
+				new MyTask().execute("");
+			else {
+				String sub = mParent.substring(0,
+						mParent.lastIndexOf(File.separator));
+				lastindex = sub.lastIndexOf(File.separator);
+				sub = sub.substring(0, lastindex + 1);
+				new MyTask().execute(sub);
+			}
 		} else
 			super.onBackPressed();
 	}
@@ -69,7 +98,7 @@ public class ZipShow extends Activity {
 			file.close();
 			return r;
 		} catch (IOException e) {
-			Log.e("dfdun", "" + e);
+			loge("" + e);
 		} finally {
 		}
 		return r;
@@ -78,13 +107,17 @@ public class ZipShow extends Activity {
 	class MyTask extends AsyncTask<String, String, String> {
 		@Override
 		protected void onPreExecute() {
-			mDirs.clear();
+//			mDirs.clear(); // 导致 mCaches无效 每次都查询
+			mDialog.setMessage("Opening ... ");
+			mDialog.show();
 		}
 
 		@Override
 		protected String doInBackground(String... as) {
-			final String parent = as[0];
-			Log.d("dfdun", "task, parent = " + parent);
+			String parent = as[0];
+			if (TextUtils.isEmpty(parent))
+				parent = sep;
+			logd("task, parent = " + parent);
 			try {
 				if (mAllDirs.isEmpty()) {
 					ZipFile zipFile = new ZipFile(mZipPath);
@@ -95,14 +128,13 @@ public class ZipShow extends Activity {
 						zipEntry = (ZipEntry) es.nextElement();
 						String s = zipEntry.getName();
 						mAllDirs.add(s);
-						Log.d("dfdun", "find directly :" + s);
+						// logd( "find directly :" + s);
 					}
 					zipFile.close();
 				}
 				mDirs = getDirX(mAllDirs, parent);
-				mDirs.remove(parent);
 			} catch (IOException e) {
-				Log.e("dfdun", "" + e);
+				loge("" + e);
 			}
 			return parent;
 		}
@@ -113,6 +145,8 @@ public class ZipShow extends Activity {
 
 		@Override
 		protected void onPostExecute(String result) {
+			if (mDialog != null && mDialog.isShowing())
+				mDialog.dismiss();
 			adapter.notifyDataSetChanged();
 			setTitle(result);
 			mParent = result;
@@ -147,24 +181,62 @@ public class ZipShow extends Activity {
 
 	};
 
+	/**
+	 * 从zip文件的所有文件列表 dirs中 筛选出目录parent下的直接文件或目录
+	 * */
 	private ArrayList<String> getDirX(ArrayList<String> dirs, String parent) {
-		if (mCaches.containsKey(parent))
+		if (mCaches.containsKey(parent)) {
 			return mCaches.get(parent);
-		final String sep = File.separator;
+		}
+
+		int index = -1;
 		ArrayList<String> childs = new ArrayList<String>();
-		if (parent == null || parent.length() == 0) {
+		if (parent == null || parent.length() == 0 || sep.equals(parent)) {
 			for (String str : dirs) {
-				if (!str.contains(sep) || str.indexOf(sep) == str.length() - 1)
+				index = str.indexOf(sep);
+				if (-1 == index || index == str.length() - 1)
 					childs.add(str);
+				else {
+					str = str.substring(0, index + 1);
+					if (!childs.contains(str))
+						childs.add(str);
+				}
 			}
-			mCaches.put("", childs);
+			mCaches.put(sep, childs);
+			logd("put key '.' ,size =" + childs.size());
 		} else {
 			for (String str : dirs) {
-				if (str.startsWith(parent))
+				if (!str.startsWith(parent))
+					continue;
+				str = str.substring(parent.length());
+				index = str.indexOf(sep);
+				if ((-1 == index || index == str.length()) && str.length() > 0) {
 					childs.add(str);
+				} else {
+					str = str.substring(0, index + 1);
+					if (!childs.contains(str) && !TextUtils.isEmpty(str)) {
+						childs.add(str);
+					}
+				}
 			}
 			mCaches.put(parent, childs);
+			logd("put key: " + parent + " ,size =" + childs.size());
 		}
 		return childs;
+	}
+
+	void logList(List<?> list) {
+		StringBuilder sb = new StringBuilder();
+		for (Object o : list)
+			sb.append(o.toString() + ",");
+		logd(sb.toString());
+	}
+
+	private void logd(String s) {
+		Log.d("orient", s);
+	}
+
+	private void loge(String e) {
+		Log.e("orient", e);
 	}
 }
